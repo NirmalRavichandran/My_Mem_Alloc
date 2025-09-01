09b1ac019d4fabf1e43fff51a455bbed4c99dfef
NirmalRavichandran <nirmal18ravi@gmail.com>
2025-09-02 01:10:20 +0530

    Stage 2: Block Header and free function Added

--- main.c
+++ main.c
@@ -1,29 +1,54 @@
 #include<stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 #define MEMORY_SIZE 1024
+
+typedef struct Block {
+    size_t size;
+    int free;
+    struct Block *next;
+} Block;
 
 char* getArray() {
     static char arr[MEMORY_SIZE];
     return arr;
 }
 
+Block* freeList = NULL;
+
+void initMemory() {
+    freeList = (Block*)getArray();
+    freeList->size = MEMORY_SIZE - sizeof(Block);
+    freeList->free = 1;
+    freeList->next = NULL;
+}
 void* allocate(size_t size) {
-    char* ptr = getArray();
-    static int free_index = 0;
-    if (free_index+size <= MEMORY_SIZE) {
-        char* a = &ptr[free_index];
-        free_index += size;
-        printf("%d",free_index);
-        return a;
-    }else {
-        return NULL;
+    Block* curr = freeList;
+    while (curr) {
+        if (curr->free && curr->size >= size) {
+            curr->free = 0;
+            return (char*)curr + sizeof(Block);
+        }
+        curr = curr->next;
     }
+    return NULL;
+}
+void freeMem(void* ptr) {
+    if (!ptr) return;
+    Block* curr = (Block*)((char*)ptr - sizeof(Block));
+    curr->free=1;
 }
 int main() {
-    char* p1 = allocate(100);
-    char* p2 = allocate(200);
-    char* p3 = allocate(800);  // should fail
-    printf("%p %p %p\n", p1, p2, p3);
+    initMemory();
+
+    char* p1 = (char*)allocate(100);
+    char* p2 = (char*)allocate(200);
+
+    printf("p1 = %p, p2 = %p\n", p1, p2);
+
+    freeMem(p1);
+    char* p3 = (char*)allocate(50); // should reuse freed block
+    printf("p3 = %p (reused p1 space)\n", p3);
+
     return 0;
 }
